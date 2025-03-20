21fdf065ee92b04207aca0f7afc4faf61f5f8fc1
Processiafr <contactprocessia@gmail.com>
2025-03-20 13:34:12 +0100

    Metadonnées et logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,14 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Finopia Academy - Business Game Expérience Immersive',
+  title: 'FINOPIA Academy',
   description: 'Venez vivre une expérience immersive et innovante pour redynamiser la formation et la montée en compétence de vos équipes. Inscrivez-vous à notre Business Game le 30 avril 2025.',
   generator: 'Next.js',
   keywords: ['formation', 'business game', 'expérience immersive', 'finance', 'supply chain', 'management', 'finopia'],
+  icons: {
+    icon: '/Logo_F_Academy.png',
+    apple: '/Logo_F_Academy.png',
+  },
 }
 
 export default function RootLayout({
